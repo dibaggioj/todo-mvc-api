8bb3acb3165d43e92220dba6ba977eae23cc0678
jsantoku <jsantoku@gmail.com>
2015-06-29 21:17:17 -0500

    - add api version

--- todo_mvc/urls.py
+++ todo_mvc/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'todo_mvc.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^api/', include('api.urls')),
+    url(r'^api/v1/', include('api.urls', namespace='v1')),
     url(r'^admin/', include(admin.site.urls)),
 )
